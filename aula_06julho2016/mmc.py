2beb37766224eecc11046d3782eaad52b6d4ce72
Rogerio Silva <rogerio.silva@ifpi.edu.br>
2016-07-06 17:03:46 -0300

    Aula 06julho2016

---
+++ aula_06julho2016/mmc.py
@@ -0,0 +1,17 @@
+#By E.
+def main():
+	num1, num2 = input('N1: '), input('N2: ')
+	resultado = mmc(num1, num2)
+	print 'MMC = %d' % resultado
+
+def mmc(numero1, numero2):
+	multiplo = 1
+
+	while multiplo % numero1 != 0 or multiplo % numero2 != 0:
+		multiplo = multiplo + 1
+
+	return multiplo
+
+
+if __name__ == '__main__':
+	main()
---
+++ aula_06julho2016/mmc_break.py
@@ -0,0 +1,18 @@
+#By E.
+def main():
+	num1, num2 = input('N1: '), input('N2: ')
+	resultado = mmc(num1, num2)
+	print 'MMC = %d' % resultado
+
+def mmc(numero1, numero2):
+	multiplo = 1 
+	
+	while True:
+		if multiplo % numero1 == 0 and multiplo % numero2 == 0:
+			return multiplo
+		else:
+			multiplo = multiplo + 1
+
+
+if __name__ == '__main__':
+	main()
